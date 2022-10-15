386f1f3be9cad5710660b435c60b187afcf7dcb2
googleplayconsole <115429501+googleplayconsole@users.noreply.github.com>
2022-10-16 03:58:45 +0500

    link to detail

--- models/rs40000Schema.js
+++ models/rs40000Schema.js
@@ -1,14 +1,45 @@
 const mongoose = require("mongoose");
 
 const Rs40000Schema = mongoose.Schema({
-      bondNumber: {
-        type: String,
-        required: [true, "Bond Number Required"],
-      },
-      link: {
-        type: String,
-        required: [true, "Link Required"],
-      }  
-});
+  drawerNo: {
+    type: String,
+    required: [true, "drawerNo Required"],
+  },
+  date: {
+    type: String,
+    required: [true, "date Required"],
+  },
+  heldAt: {
+    type: String,
+    required: [true, "heldAt Required"],
+  },
+  drawerOf: {
+    type: String,
+    required: [true, "drawerOf Required"],
+  },
+  drawerNo1: {
+    type: String,
+    required: [true, "drawerNo1 Required"],
+  },
+  drawerNo2: {
+    type: String,
+    required: [true, "drawerNo2 Required"],
+  },
+  drawerNo3: {
+    type: String,
+    required: [true, "drawerNo3 Required"],
+  },
+  drawerList1: [{
+    type: String,
+    required: [true, "drawerList1 Required"],
+  }],
+  drawerList2: [{
+    type: String,
+    required: [true, "drawerList2 Required"],
+  }],
+  drawerList3: [{
+    type: String,
+    required: [true, "drawerList3 Required"],
+  }],});
 
-module.exports = mongoose.model("rs40000datas", Rs40000Schema);
+module.exports = mongoose.model("rs40000s", Rs40000Schema);
